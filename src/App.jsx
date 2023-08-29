9d54eab635a988b82f80a1f57c3fcdde3c1750a0
Nilton César <104658639+niltoncesar3@users.noreply.github.com>
2023-08-29 20:01:58 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,6 @@ import { login } from './utils';
 import './index.css';
 import { useState } from 'react';
 
-// Instruções:
-// * Você tem um formulário de login INCOMPLETO
-// * Não é permitido adicionar novos elementos HTML
-// * Não é permitido usar refs
-//
-// Tarefas:
-// todo - Desabilite o botão de Login caso o e-mail esteja em branco OU a senha for menor que 6 dígitos. 
-// todo - O botão de login deve disparar a função login(), importada no topo deste arquivo, e passar os dados necessários.
-// todo - Desabilite o botão de Login equanto você está executando o login.
-// todo - Mostre uma mensagem de erro de login() caso o Login falhe. A mensagem deve ser limpa a cada nova tentativa de Login.
-// todo - Mostre um alerta caso o login seja efetuado com sucesso (javascript alert). Investigue a função login() para entender como ter sucesso na requisição.
 
 export default function LoginForm() {
 
